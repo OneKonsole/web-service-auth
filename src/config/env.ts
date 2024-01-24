c82ec4c46b3021eeb4c6ab5f9905d65a76785116
Teissier Yannis <yannis.teissier70@gmail.com>
2024-01-24 14:32:32 +0100

    Develop (#8)
    * Init commit
    
    * Start deploy
    
    * Create main.yml
    
    * Update main.yml
    
    * Update dockerfile
    
    * Feature/deploy (#7)
    
    * Add env to auth deploy
    
    * Update values.yaml
    
    * test :D
    
    * remove invalid conf
    
    * fix image
    
    * Trying to fix cicd and add some logs
    
    * Trying to fix cicd and add some logs
    
    * Trying to fix cicd and add some logs
    
    * Fix deploy adn server
    
    * move ctx
    
    * Add /verify
    
    * fix: helm chart issues
    
    ---------
    
    Co-authored-by: EBMBA <e.b.metral@gmail.com>
    
    ---------
    
    Co-authored-by: EBMBA <e.b.metral@gmail.com>

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,30 +1,34 @@
 // src/config/env.ts
-export const PORT = process.env.PORT || 80;
+const verify = (value: any) => {
+    // check if null, empty or equal to ''
+    return !(value == null || value === '' || value === undefined);
+}
 
-export const DB_USERNAME = process.env.DB_USERNAME || '';
+export const PORT = verify(process.env.PORT) ? process.env.PORT: 80;
+export const DB_USERNAME: string  = verify(process.env.DB_USERNAME) ? process.env.DB_USERNAME as string : '';
 
-export const DB_PASSWORD = process.env.DB_PASSWORD || '';
+export const DB_PASSWORD: string = verify(process.env.DB_PASSWORD) ? process.env.DB_PASSWORD as string : '';
 
-export const DB_HOST = process.env.DB_HOST || '';
+export const DB_HOST: string = verify(process.env.DB_HOST) ? process.env.DB_HOST as string : '';
 
-export const DB_NAME = process.env.DB_NAME || '';
+export const DB_NAME: string = verify(process.env.DB_NAME) ? process.env.DB_NAME as string : '';
 
-export const DB_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?appName=mongosh+1.9.1&retryWrites=true&w=majority`;
+export const DB_URL: string = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?appName=mongosh+1.9.1&retryWrites=true&w=majority`;
 
 // You should ideally store the JWT secret in an environment variable for security purposes
-export const JWT_SECRET = process.env.JWT_SECRET || '';
+export const JWT_SECRET: string = verify(process.env.JWT_SECRET) ? process.env.PORT as string: 'SECRET';
 
-export const JWT_EXPIRATION = process.env.JWT_EXPIRATION || '';
+export const JWT_EXPIRATION: string = verify(process.env.JWT_EXPIRATION) ? process.env.JWT_EXPIRATION as string : '';
 
 // Keycloak
-export const KEYCLOAK_REALM = process.env.KEYCLOAK_REALM || '';
+export const KEYCLOAK_REALM: string = verify(process.env.KEYCLOAK_REALM) ? process.env.KEYCLOAK_REALM as string : '';
 
-export const KEYCLOAK_SERVER_URL = process.env.KEYCLOAK_SERVER_URL || '';
+export const KEYCLOAK_SERVER_URL: string = verify(process.env.KEYCLOAK_SERVER_URL) ? process.env.KEYCLOAK_SERVER_URL as string : '';
 
-export const KEYCLOAK_CLIENT_ID = process.env.KEYCLOAK_CLIENT_ID || '';
+export const KEYCLOAK_CLIENT_ID: string = verify(process.env.KEYCLOAK_CLIENT_ID) ? process.env.KEYCLOAK_CLIENT_ID as string : '';
 
-export const KEYCLOAK_CLIENT_SECRET = process.env.KEYCLOAK_CLIENT_SECRET || '';
+export const KEYCLOAK_CLIENT_SECRET: string = verify(process.env.KEYCLOAK_CLIENT_SECRET) ? process.env.KEYCLOAK_CLIENT_SECRET as string : '';
 
-export const KEYCLOAK_ADMIN_USERNAME = process.env.KEYCLOAK_ADMIN_USERNAME || '';
+export const KEYCLOAK_ADMIN_USERNAME: string = verify(process.env.KEYCLOAK_ADMIN_USERNAME) ? process.env.KEYCLOAK_ADMIN_USERNAME as string : '';
 
-export const KEYCLOAK_ADMIN_PASSWORD = process.env.KEYCLOAK_ADMIN_PASSWORD || '';
+export const KEYCLOAK_ADMIN_PASSWORD: string = verify(process.env.KEYCLOAK_ADMIN_PASSWORD) ? process.env.KEYCLOAK_ADMIN_PASSWORD as string : '';
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -3,18 +3,70 @@ import express from 'express';
 import ClassicUserController from "../controllers/userController";
 import {IUserController} from "../controllers/IUserController";
 import {authenticate} from '../middlewares/middlewares';
-import APIUserController from "../controllers/userController.api"; // Classic
+import APIUserController from "../controllers/userController.api";
+import {LOG_TYPE, logger} from "../utils/logger"; // Classic
 // API token verification
 
 const router = express.Router();
 
 const userController: IUserController = new APIUserController();
 
-router.post('/users/register', (req, res) => userController.createUser(req, res));
-router.post('/users/logout', authenticate, (req, res) => userController.logout(req, res));
-router.post('/users/login', (req, res) => userController.login(req, res));
-router.post('/users/refresh', (req, res) => userController.refreshToken(req, res));
-router.get('/users/:userUuid', authenticate, (req, res) => userController.getUser(req, res));
-router.put('/users/:userUuid', authenticate, (req, res) => userController.updateUser(req, res));
-router.delete('/users/:userUuid', authenticate, (req, res) => userController.deleteUser(req, res));
+router.get('/', (req, res) => {
+    // liveness probe
+    res.status(200).json({message: 'OK'});
+});
+
+router.post('/users/register', (req, res) => {
+    try {
+        return userController.createUser(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.post('/users/logout', authenticate, (req, res) => {
+    try {
+        return userController.logout(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.post('/users/login', (req, res) => {
+    try {
+        return userController.login(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.post('/users/refresh', (req, res) => {
+    try {
+        return userController.refreshToken(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.get('/users/:userUuid', authenticate, (req, res) => {
+    try {
+        return userController.getUser(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.get('/verify', authenticate, (req, res) => {
+    res.status(200).json({message: 'OK'});
+})
+
+router.put('/users/:userUuid', authenticate, (req, res) => {
+    try {
+        return userController.updateUser(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
+router.delete('/users/:userUuid', authenticate, (req, res) => {
+    try {
+        return userController.deleteUser(req, res)
+    } catch (e: any) {
+        logger(e.message, LOG_TYPE.ERROR);
+    }
+});
 export default router;
